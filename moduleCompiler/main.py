d23e9e6d5fb672200242df3f044efe9f13a4a95e
adamjhawley <adamhawley99@gmail.com>
2019-03-31 16:15:07 +0000

    Created output file generation and preamble extraction

--- moduleCompiler/main.py
+++ moduleCompiler/main.py
@@ -5,6 +5,26 @@ import shutil
 
 from collect_files import filter_tex_files
 
+def extract_preamble(tex_paths):
+    preamble = []
+    contents = ['\\begin{document}\n']
+    for path in tex_paths:
+        f = open(path, "r")
+        lines = f.readlines()
+        
+        for index, line in enumerate(lines):
+            if line == "\\begin{document}\n":
+                doc_start = index + 1
+                break
+            elif line not in preamble:
+                preamble.append(line)
+
+        for line in lines[doc_start:-1]:
+            contents.append(line)
+
+    contents.append('\\end{document}\n')
+    return preamble, contents
+
 def main():
     try:
         path = sys.argv[1]
@@ -12,19 +32,22 @@ def main():
         path = None
 
     tex_files = filter_tex_files(path)
+    copies = []
 
     for path in tex_files:
-        copy_path = path[:-4] + "COPY.tex"
+        copy_path = path[:-4] + "_COPY.tex"
+        copies.append(copy_path)
         shutil.copyfile(path, copy_path)
 
-    file_contents = []
+    preamble, file_contents = extract_preamble(copies)
 
-    print(tex_files)
-
-    sleep(5)
-    print("Deleting")
+    output = open('output.tex', 'w')
+    output.writelines(preamble)
+    output.writelines(file_contents)
+    
+    #Remove copied files
     for path in tex_files:
-        os.remove(path[:-4] + "COPY.tex")
+        os.remove(path[:-4] + "_COPY.tex")
 
 if __name__ == '__main__':
     main()
